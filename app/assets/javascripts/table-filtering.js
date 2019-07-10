c673368e1175648de8f2c137f1c0cd3477e70d01
Ros <YOUR EMAIL ADDRESS>
2019-07-10 13:57:31 +0100

    Table filtering now checkboxes

--- app/assets/javascripts/table-filtering.js
+++ app/assets/javascripts/table-filtering.js
@@ -8,9 +8,19 @@
       var cells = row.querySelectorAll('td')
 
       columns.forEach(function(columnIndex) {
-        if ( cells[columnIndex].textContent.toLowerCase().indexOf(value.toLowerCase()) !== -1 ) {
-          hideRow = false
+        // value might be an array of values...
+        if( Array.isArray(value)) {
+          value.forEach(function(item) {
+            if ( cells[columnIndex].textContent.toLowerCase().indexOf(item.toLowerCase()) !== -1 ) {
+              hideRow = false
+            }
+          })
+        } else {
+          if ( cells[columnIndex].textContent.toLowerCase().indexOf(value.toLowerCase()) !== -1 ) {
+            hideRow = false
+          }
         }
+
       })
 
       if (hideRow) {
@@ -25,13 +35,13 @@
     })
   }
 
-  function hideViewedRows() {
-    [].forEach.call(rows, function(row, index) {
-      if (row.classList.contains('viewed')) {
-        row.classList.add('govuk-visually-hidden')
-      }
-    })
-  }
+  // function hideViewedRows() {
+  //   [].forEach.call(rows, function(row, index) {
+  //     if (row.classList.contains('viewed')) {
+  //       row.classList.add('govuk-visually-hidden')
+  //     }
+  //   })
+  // }
 
   // Bind filtering functionality to search box
   var searchBox = document.getElementById('search-input')
@@ -41,16 +51,49 @@
   var hideViewed = document.getElementById('hide-viewed')
   searchForm.addEventListener('submit', function(e) {
     e.preventDefault()
-    resetTable()
-    filterTable(applicationType.value, [3])
-    filterTable(applicationProgress.value, [5])
-    filterTable(searchBox.value, [1, 2])
+    updateSearch()
+  })
 
-    if (hideViewed.checked) {
-        hideViewedRows()
+  // Change handler on the checkboxes
+  applicationProgressCheckboxes = Array.prototype.slice.call(document.querySelectorAll('[name="application-progress"]'));
+  applicationTypeCheckboxes = Array.prototype.slice.call(document.querySelectorAll('[name="application-type"]'));
+  applicationProgressCheckboxes.concat(applicationTypeCheckboxes).forEach(function(checkbox, index) {
+    checkbox.addEventListener('change', function(e) {
+      updateSearch()
+    })
+  })
+
+  function getSelectedCheckboxValues(checkboxes) {
+    return checkboxes.reduce(function(accumulator, currentCheckbox, currentIndex, array) {
+      if(currentCheckbox.checked) {
+          return accumulator.concat(currentCheckbox.value)
+      } else {
+          return accumulator
+      }
+    }, [])
+  }
+
+  function updateSearch() {
+    resetTable()
+
+    const applicationProgressChoices = getSelectedCheckboxValues(applicationProgressCheckboxes)
+    if (applicationProgressChoices.length > 0) {
+        filterTable(applicationProgressChoices, [5])
     }
 
-  })
+    const applicationTypeChoices = getSelectedCheckboxValues(applicationTypeCheckboxes)
+    if (applicationTypeChoices.length > 0) {
+        filterTable(applicationTypeChoices, [3])
+    }
+
+    if(searchBox.value.length > 0) {
+      filterTable(searchBox.value, [1, 2])
+    }
+
+    // if (hideViewed.checked) {
+    //     hideViewedRows()
+    // }
+  }
 
 
   // Reset link
@@ -60,8 +103,9 @@
     resetTable()
 
     searchBox.value = ''
-    applicationType.value = ''
-    applicationProgress.value = ''
-    hideViewed.checked = false
+    applicationTypeCheckboxes.concat(applicationProgressCheckboxes).forEach(function(checkbox) {
+      checkbox.checked = false
+    })
+    // hideViewed.checked = false
   })
 })()
