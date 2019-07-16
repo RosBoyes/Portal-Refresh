1869c7bc153f431557c358c5bf455ca6960d3247
Ros <YOUR EMAIL ADDRESS>
2019-07-16 10:01:29 +0100

    Edits to my work + filtering and more

--- app/assets/javascripts/table-filtering.js
+++ app/assets/javascripts/table-filtering.js
@@ -109,3 +109,19 @@
     // hideViewed.checked = false
   })
 })()
+
+
+var coll = document.getElementsByClassName("filter");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
